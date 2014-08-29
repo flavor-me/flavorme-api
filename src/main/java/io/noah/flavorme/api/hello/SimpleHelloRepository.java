0be6af75ffcf18dfd765dbbd34f52c5f9fa5d1fe
chanwook <chanwook.god@gmail.com>
2014-08-29 12:56:54 +0900

    클래스가 누락되어 다시 커밋함!!
    왜 누락???

---
+++ src/main/java/io/noah/flavorme/api/hello/SimpleHelloRepository.java
@@ -0,0 +1,45 @@
+package io.noah.flavorme.api.hello;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Repository;
+
+import static org.springframework.data.mongodb.core.query.Criteria.where;
+import static org.springframework.data.mongodb.core.query.Query.query;
+
+/**
+ * Created by chanwook on 2014. 8. 28..
+ */
+@Repository
+public class SimpleHelloRepository implements HelloRepository {
+
+    @Autowired
+    private MongoTemplate mt;
+
+
+    @Override
+    public void hello(Message msg) {
+        mt.insert(msg);
+    }
+
+    @Override
+    public Message find(String id) {
+        return mt.findById(id, Message.class);
+    }
+
+    @Override
+    public void bye(Message msg) {
+        mt.remove(msg);
+    }
+
+    @Override
+    public void update(Message msg) {
+        mt.updateFirst(query(where("id").is(msg.getId())), Update.update("message", msg.getMessage()), Message.class);
+    }
+
+    @Override
+    public int countAll() {
+        return mt.findAll(Message.class).size();
+    }
+}
